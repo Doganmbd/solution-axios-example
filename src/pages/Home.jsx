83d9efd56bff6929986ddbd5a24456aeda625da8
mbd <doganmbdogan@gmail.com>
2023-05-29 16:07:40 +0300

    4 - GET işlemi yapıldı

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import TutorialList from "../components/TutorialList";
 import AddTutorial from "../components/AddTutorial";
+import axios from "axios";
 
 const Home = () => {
+  const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  //! GET (read işlemi)
+  const getUrl = async () => {
+    /* Axios objeleri data adında bir yerde saklıyor. */
+    try {
+          const { data } = await axios.get(url);
+    console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+  
+  };
+
+  getUrl();
+
   return (
     <div>
       <AddTutorial />
