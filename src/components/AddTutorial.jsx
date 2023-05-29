29e9f4b2d7f048a7152a67ce89a744014147ee41
mbd <doganmbdogan@gmail.com>
2023-05-29 17:08:21 +0300

    9 - inpulara girilen veriyi USESTATE ile tuttum.

--- src/components/AddTutorial.jsx
+++ src/components/AddTutorial.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTutorial = () => {
+  const [addTitle, setAddTitle] = useState("");
+  const [addDescription, setAddDescription] = useState("");
+/*   console.log(addTitle);
+  console.log(addDescription); */
   return (
     <div>
       <form>
@@ -13,13 +17,21 @@ const AddTutorial = () => {
             className="form-control"
             id="title"
             aria-describedby="emailHelp"
+            onChange={(e) => setAddTitle(e.target.value)}
+            value={addTitle}
           />
         </div>
         <div className="mb-3">
           <label htmlFor="desc" className="form-label">
             Description
           </label>
-          <input type="text" className="form-control" id="desc" />
+          <input
+            type="text"
+            className="form-control"
+            id="desc"
+            onChange={(e) => setAddDescription(e.target.value)}
+            value={addDescription}
+          />
         </div>
         <div className="mb-5 text-center">
           <button type="submit" className="btn btn-primary  text-center">
