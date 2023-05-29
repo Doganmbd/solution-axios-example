d2f1494377f1d0f46d10671b8d085b6724185f51
mbd <doganmbdogan@gmail.com>
2023-05-29 16:34:42 +0300

    6- UseEffect ve UseState kullanıldı

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TutorialList from "../components/TutorialList";
 import AddTutorial from "../components/AddTutorial";
 import axios from "axios";
 
 const Home = () => {
+  const [apiData, setApiData] = useState();
+
   const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
 
   //! GET (read işlemi)
   const getUrl = async () => {
-    /* Axios objeleri data adında bir yerde saklıyor. */
+    /* Axios objeleri data adında bir yerde saklıyor. Bu yüzden contructor yapıp çıkartıyoruz.*/
     try {
-          const { data } = await axios.get(url);
-    console.log(data);
+      const { data } = await axios.get(url);
+      setApiData(data);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-  
   };
 
-  getUrl();
+  //! UseState içine URL den gellen verileri gönderdiğim için sürekli render işlemi olmaması için USEEFFECT kullanarak MOUNTED yaptım. 
+  useEffect(() => {
+    getUrl();
+  }, []);
 
+  /*  console.log(apiData);  */
+  
   return (
     <div>
       <AddTutorial />
